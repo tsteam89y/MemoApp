98c608161c6c8b93acdef392dda1e7616021d853
tsteam89y <8735641+tsteam89y@users.noreply.github.com>
2023-12-04 00:52:07 +0900

    Styling add button.

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -44,8 +44,8 @@ const Index = (): JSX.Element => {
 
       </View>
 
-      <View>
-        <Text>+</Text>
+      <View style={styles.circleButton}>
+        <Text style={styles.circleButtonLabel}>+</Text>
       </View>
     </View>
   )
@@ -95,6 +95,27 @@ const styles = StyleSheet.create({
     fontSize: 12,
     lineHeight: 16,
     color: '#848484'
+  },
+  circleButton: {
+    width: 64,
+    height: 64,
+    borderRadius: 32,
+    backgroundColor: '#467FD3',
+    justifyContent: 'center',
+    alignItems: 'center',
+    position: 'absolute',
+    right: 40,
+    bottom: 40,
+    shadowColor: '#000000',
+    shadowOpacity: 0.25,
+    shadowRadius: 8,
+    shadowOffset: { width: 0, height: 8 },
+    elevation: 8
+  },
+  circleButtonLabel: {
+    color: '#FFFFFF',
+    fontSize: 40,
+    lineHeight: 40
   }
 })
 
